fd16f96f3c2ce29b253c6dde25de6d7189ad8e0b
Mathieu <mathieu.bue@edu.devinci.fr>
2021-01-09 19:52:25 +0100

    base game

---
+++ Part3/Part3/Board.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part3
+{
+	class Board
+	{
+		private Cell start;
+
+		public Board()
+		{
+			this.start = null;
+		}
+
+		public Cell Start
+		{
+			get { return this.start; }
+			set { this.start = value; }
+		}
+
+
+		
+		public Cell initBoard()
+		{
+			this.start = new Cell(0);
+			Cell curr = this.start;
+			int i = 1;
+			while(i<40)
+			{
+				Cell nextcell = new Cell(i);
+				curr.Next = nextcell;
+				curr = curr.Next;
+				i++;
+			}
+			curr.Next = this.start;
+			return this.start;
+		}
+	}
+}
---
+++ Part3/Part3/Cell.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part3
+{
+	class Cell
+	{
+		private int value;
+		private Cell next;
+
+		public Cell(int value)
+		{
+			this.value = value;
+			this.next = null;
+		}
+
+		public int Value
+		{
+			get { return this.value; }
+			set { this.value = value; }
+		}
+		public Cell Next
+		{
+			get { return this.next; }
+			set { this.next = value; }
+		}
+		public override string ToString()
+		{
+			return "Numero : " +  this.value.ToString();
+		}
+	}
+}
